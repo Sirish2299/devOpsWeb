198e7644cdd8a756cf79aa545f74f7686095d06b
Sirish2299 <96455092+Sirish2299@users.noreply.github.com>
2022-09-15 23:00:10 +0530

    Add files via upload

---
+++ JenkinsFile.groovy
@@ -0,0 +1,23 @@
+pipeline{
+     agent any
+
+     stages{
+          stage ('Build') {
+               steps{
+                    sh 'mvn clean package'
+               }
+               post{
+                    succes{
+                         echo "Archiving the Artificats"
+                         archiveArtificats artificats: '**/target/*.war'
+                    }
+               }
+          }
+          stage ('Deploy to tomcat server') {
+               steps{
+                    deploy adapters: [tomcat8(credentialsId: 'TomcatCreds', path: '', url: 'http://localhost:9010/')], contextPath: null, war: '**/*.war'
+               }
+
+          }
+     }
+}
